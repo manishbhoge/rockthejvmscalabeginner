5ec275af56f1a4e45bba7a4bb237f865277e174a
Manish Bhoge <bhoge.m@outlook.com>
2020-05-25 18:08:53 +0530

    Commit: Expression and Instruction

--- src/lecture/part2/op/OBasics.scala
+++ src/lecture/part2/op/OBasics.scala
@@ -1,8 +1,12 @@
 package lecture.part2.op
 
 object OBasics extends App {
-val person = new Person
-  println(person)
+val person = new Person("John", age = 26)
+  println(person.age)
 }
 
-class Person
+class Person(Name: String, val age: Int) // Constructor
+
+// Class parameter are not field or member
+// Class parameter and class field are two different things in Scala
+
