631f66a85a76e11ad9e24b1fb1ec2a15c51b10ab
Manish Bhoge <bhoge.m@outlook.com>
2020-05-25 13:56:30 +0530

    Commit: Expression and Instruction

--- src/lecture/part1basics/Expressions.scala
+++ src/lecture/part1basics/Expressions.scala
@@ -8,6 +8,32 @@ object Expressions extends App {
   println(2+3*4)
 
   println(1==x)
+  println(!(2==x))
+
+  // instruction (Do) v/s expression (Value)
+
+  val aCondition=true
+
+  val aConditionVal = if(aCondition) 5 else 10
+
+  println(aConditionVal)
+
+  // Everything in Scala is an expression
+  var aVariable=0
+  val aWeirdVal = (aVariable==3)
+  println(aWeirdVal)
+
+  val aWhile = while(aVariable<=9){
+    // println(aVariable)
+    aVariable+=1
+  }
+
+  println(aWhile)
+
+  val someValue = {
+  2<3
+}
+  println(someValue)
 
 
 }
